0dadcdc1519d0278b037310c2fd431efedc33c3d
eugcapt <zhk.sem@yandex.ru>
2022-10-01 13:25:49 +0300

    Modified navigation

--- app/src/main/java/com/eugcapt/soccerquiz/GoalFragment.kt
+++ app/src/main/java/com/eugcapt/soccerquiz/GoalFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import com.eugcapt.soccerquiz.databinding.FragmentGoalBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -34,7 +37,11 @@ class GoalFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_goal, container, false)
+        val binding = DataBindingUtil.inflate<FragmentGoalBinding>(inflater, R.layout.fragment_goal, container, false)
+        binding.oneMoreTimeButton.setOnClickListener { view: View ->
+            view.findNavController().navigate(R.id.action_goalFragment2_to_quizFragment)
+        }
+        return binding.root
     }
 
     companion object {
--- app/src/main/java/com/eugcapt/soccerquiz/MissFragment.kt
+++ app/src/main/java/com/eugcapt/soccerquiz/MissFragment.kt
@@ -5,6 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import com.eugcapt.soccerquiz.databinding.FragmentGoalBinding
+import com.eugcapt.soccerquiz.databinding.FragmentMissBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -33,8 +37,11 @@ class MissFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_miss, container, false)
+        val binding = DataBindingUtil.inflate<FragmentMissBinding>(inflater, R.layout.fragment_miss, container, false)
+        binding.oneMoreTimeButton.setOnClickListener { view: View ->
+            view.findNavController().navigate(R.id.action_missFragment_to_quizFragment)
+        }
+        return binding.root
     }
 
     companion object {
